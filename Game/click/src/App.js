21c4bee9aec3bc675545369dd36164459332eada
Colby Schumm <coolbreeze@Colbys-MacBook-Air.local>
2018-10-03 13:53:01 -0600

    first push

---
+++ Game/click/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Redhead from './components/Redhead';
+import Title from './components/Title';
+import Wrapper from './components/Wrapper;';
+import Redheads from './Redheads.json';
+import './App.css';
+
+
+
+
+class App extends Component {
+ state = {
+    score: 0,
+    highscore:0,
+    message:"Pick a Redhead to start",
+    Redheads
+ };
+}
+
+export default App;
